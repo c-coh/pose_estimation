ce98066025ea28f5cd269a338c795a1c2777e11f
Ceci Cohen <119373344+c-coh@users.noreply.github.com>
2024-08-12 18:19:32 -0400

    add basic functions

--- pose_estimation.py
+++ pose_estimation.py
@@ -1,7 +1,44 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
-img = cv.imread('image.png')
-cv.imshow('image', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+import numpy as np
+from IPython.display import Image
+import time
+import sys
 
+
+def checkerboard():
+
+    # start camera stream ()
+
+
+def camera():
+    source = cv.VideoCapture(0)
+
+    cv.namedWindow('camera feed', cv.WINDOW_NORMAL)
+
+    while cv.waitKey(5) != 27:  # Escape key
+        has_frame, frame = source.read()
+        if not has_frame:
+            break
+        cv.imshow('camera feed', frame)
+
+    source.release()
+    cv.destroyWindow('camera feed')
+
+
+# get numeric array corresponding to image file
+def printImgArr(img):
+    # set print option to show entire array
+    np.set_printoptions(threshold=np.inf)
+
+    img = cv.imread(img, 0)
+
+    # write to file
+    f = open("output.txt", "w")
+    f.write(np.array2string(img))
+    f.close()
+
+
+####    MAIN    ####
+# printImgArr('image.png')
+camera()
