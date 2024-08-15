581cf837f04be564d2694ed411aef034bbedc7cd
Ceci Cohen <119373344+c-coh@users.noreply.github.com>
2024-08-15 00:11:08 -0400

    Find chessboard corners

--- pose_estimation.py
+++ pose_estimation.py
@@ -4,11 +4,97 @@ import numpy as np
 from IPython.display import Image
 import time
 import sys
+import glob
 
 
+def findChessboardStatic(img):
+    chessboard_size = (12, 8)
+    image = cv.imread(img)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    cv.imshow(image)
+    cv.waitKey(0)
+
+    ret, corners = cv.findChessboardCorners(gray, chessboard_size, None)
+    print(ret)
+    if ret:
+        corners = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+
+        cv.drawChessboardCorners(image, chessboard_size, corners, ret)
+        cv.imshow(image)
+        cv.waitKey(0)
+
+    cv.destroyAllWindows()
+
+
+def findChessboard():
+    chessboard_size = (12, 8)
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    cap = cv.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        ret, corners = cv.findChessboardCorners(gray, chessboard_size, None)
+
+        if ret:
+            corners = cv.cornerSubPix(
+                gray, corners, (11, 11), (-1, -1), criteria)
+            cv.drawChessboardCorners(frame, chessboard_size, corners, ret)
+
+        cv.imshow(frame)
+
+        if cv.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+
+
+# TODO Modify code
+""""
+    camera calibration used to correct distortion, radial or tangential. Must find 5 distortion coefficients (k1 k2 p1 p2 k3)
+
+    Camera matrix stores parameters of camera: focal length and optical centers
+"""
+
+
+def cameraCalibration():
+    # termination criteria
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*7, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+    # Arrays to store object points and image points from all the images.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
+    images = glob.glob('*.jpg')
+    for fname in images:
+        img = cv.imread(fname)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv.cornerSubPix(
+                gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+            # Draw and display the corners
+            cv.drawChessboardCorners(img, (7, 6), corners2, ret)
+            cv.imshow('img', img)
+            cv.waitKey(500)
+    cv.destroyAllWindows()
+
+
+# TODO Modify code
 def checkerboard():
-
-    # start camera stream ()
+    null
 
 
 def camera():
@@ -16,7 +102,7 @@ def camera():
 
     cv.namedWindow('camera feed', cv.WINDOW_NORMAL)
 
-    while cv.waitKey(5) != 27:  # Escape key
+    while cv.waitKey(5) != 27:
         has_frame, frame = source.read()
         if not has_frame:
             break
@@ -26,14 +112,11 @@ def camera():
     cv.destroyWindow('camera feed')
 
 
-# get numeric array corresponding to image file
 def printImgArr(img):
-    # set print option to show entire array
     np.set_printoptions(threshold=np.inf)
 
     img = cv.imread(img, 0)
 
-    # write to file
     f = open("output.txt", "w")
     f.write(np.array2string(img))
     f.close()
@@ -41,4 +124,4 @@ def printImgArr(img):
 
 ####    MAIN    ####
 # printImgArr('image.png')
-camera()
+findChessboard()
